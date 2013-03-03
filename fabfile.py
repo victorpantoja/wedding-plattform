8a632dac0734664da343295fbf79e3bc6e7180e1
Victor Pantoja <victor.pantoja@gmail.com>
2013-03-03 01:27:52 -0300

    deploy conf files

---
+++ fabfile.py
@@ -0,0 +1,118 @@
+# coding: utf-8
+import os
+
+import boto
+from fabric.api import *
+
+LOCAL_DIR = os.path.dirname(__file__)
+boto_config_file = os.path.join(LOCAL_DIR, 'deploy', 'boto.cfg')
+if not os.path.exists(boto_config_file):
+    abort("Please create deploy/boto.cfg file with AWS credentials")
+boto.config.load_from_path(boto_config_file)
+
+# we need to import this one after boto.config.load_from_path
+from fabix.aws import ec2
+
+
+PROJECT_DIR = '/var/www/project/'
+
+
+@task
+def prod():
+    """Configure env variables for production"""
+    env.user = 'ubuntu'
+    env.hosts = ['ec2-107-21-234-26.compute-1.amazonaws.com']
+
+
+@task
+def setup():
+    """Setup project"""
+    packages = 'nginx python python-pip'
+
+    with prefix('DEBIAN_FRONTEND=noninteractive'):
+        sudo('apt-get update')
+        sudo('apt-get -y install {}'.format(packages))
+
+    sudo('mkdir -p {}'.format(PROJECT_DIR))
+    sudo('mkdir -p /var/log/wedding_plattform')
+
+    sudo('pip install virtualenv')
+    sudo('virtualenv {}virtualenv'.format(PROJECT_DIR))
+
+    install_requirements()
+    put(os.path.join(LOCAL_DIR, 'deploy', 'upstart.conf'),
+        '/etc/init/wedding_plattform.conf', use_sudo=True)
+    put(os.path.join(LOCAL_DIR, 'deploy', 'nginx-site.conf'),
+        '/etc/nginx/sites-enabled/wedding_plattform.conf', use_sudo=True)
+    sudo('rm -f /etc/nginx/sites-enabled/default')
+    execute(nginx, 'restart')
+
+
+def install_requirements():
+    requirements = os.path.join(LOCAL_DIR, 'requirements.txt')
+    python_packages = open(requirements).read().replace('\n', ' ')
+    sudo('{}virtualenv/bin/pip install {}'.format(PROJECT_DIR, python_packages))
+
+
+@task
+def setup_autoscale():
+    """Setup AWS autoscale"""
+    my_id = 'wedding_plattform'
+
+    kwargs = {
+        "ami_id": 'ami-3fec7956',  # Official Ubuntu 12.04.1 LTS US-EAST-1
+        "instance_type": "t1.micro",
+        "key_name": my_id,
+        "security_groups": [my_id],
+        "availability_zones": ["us-east-1a", "us-east-1b", "us-east-1d"],
+        "min_instances": 0,
+        "sp_up_adjustment": 1,
+        "load_balancers": [my_id]
+    }
+    ec2.setup_autoscale(my_id, **kwargs)
+
+
+@task
+def update_autoscale(instance_id):
+    """Update autoscale configuration"""
+    ec2.update_autoscale(instance_id, 'wedding_plattform')
+
+
+@task
+def deploy():
+    """Deploy project to server"""
+    sudo('rm -rf {}wedding_plattform'.format(PROJECT_DIR))
+    put(os.path.join(LOCAL_DIR, 'wedding_plattform'), PROJECT_DIR, use_sudo=True)
+
+    # move settings
+    with cd("{}wedding_plattform/".format(PROJECT_DIR)):
+        sudo('mv settings_prod.py settings.py')
+        sudo('rm settings.pyc')
+
+    execute(restart)
+
+
+@task
+def start():
+    """Start application service"""
+    sudo('start wedding_plattform')
+
+
+@task
+def stop():
+    """Stop application service"""
+    sudo('stop wedding_plattform')
+
+
+@task
+def restart():
+    """Stop and start application service"""
+    with settings(warn_only=True):
+        execute(stop)
+    execute(start)
+
+
+@task
+def nginx(op):
+    """Manage nginx service"""
+    sudo('service nginx {}'.format(op))
---
+++ wedding_plattform/settings_prod.py
@@ -0,0 +1,17 @@
+from settings_base import *
+
+PORT = 9080
+
+LOGGING['root']['handlers'] = ['file']
+LOGGING['handlers']['file'] = {
+    'level': 'INFO',
+    'class': 'logging.FileHandler',
+    'formatter': 'detailed',
+    'filename': '/var/log/wedding_plattform/app.log',
+    'encoding': 'utf-8',
+}
+
+MEMCACHE = {
+    'servers': ('wedding_plattform.3dpyc9.cfg.use1.cache.amazonaws.com:11211',),
+    'socket_timeout': 1,
+}
